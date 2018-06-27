aafcb47240ed0f64cd6210c8fbf837619af2dc5d
Jayden Seric <me@jaydenseric.com>
2018-06-27 17:57:40 +1000

    Fix up eslint-config-env.

--- index.js
+++ index.js
@@ -1,11 +1,14 @@
 var emitter = require('tiny-emitter/instance')
 var Router = require('next/router').default
-;[
+
+var routerEvents = [
   'routeChangeStart',
   'routeChangeComplete',
   'routeChangeError',
   'beforeHistoryChange'
-].forEach(function(event) {
+]
+
+routerEvents.forEach(function(event) {
   Router['on' + event[0].toUpperCase() + event.slice(1)] = function() {
     emitter.emit.bind(emitter, event).apply(null, arguments)
   }
